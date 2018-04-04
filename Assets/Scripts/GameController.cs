c58d7449e56890ddf2b0983577430f8b05239fbe
rkolen <rkolen2007@gmail.com>
2018-04-04 10:03:47 +0200

    first unity game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject ball;
+    public float timeLeft;
+    public Text timerText;
+    public GameObject gameOverText;
+    public GameObject restartButton;
+    public GameObject splashScreen;
+    public GameObject startButton;
+    public HatController hatController;
+
+    private float maxWidth;
+    private bool playing;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        playing = false;
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, z: 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - ballWidth;
+        UpdateText();
+    }
+
+    void FixedUpdate()
+    {
+        if (playing)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                timeLeft = 0;
+            }
+            UpdateText();
+        }
+    }
+
+    public void StartGame()
+    {
+        splashScreen.SetActive(false);
+        startButton.SetActive(false);
+        hatController.ToggleControl(true);
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn ()
+    {
+        while (timeLeft > 0)
+        {
+            yield return new WaitForSeconds(2.0f);
+            playing = true;
+            Vector3 spawnPosition = new Vector3(
+                Random.Range(-maxWidth, maxWidth),
+                transform.position.y,
+                0.0f
+                );
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(ball, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(0.01f, 0.02f));
+        }
+        yield return new WaitForSeconds(2.0f);
+        gameOverText.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        restartButton.SetActive(true);
+    }
+
+    void UpdateText()
+    {
+        timerText.text = "Time Left:\n" + Mathf.RoundToInt(timeLeft);
+    }
+}
